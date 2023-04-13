04be22259c549d2928332aeba081a4c34b27f4d8
Michelle Gomez <themichelmoza@gmail.com>
2023-04-12 23:25:20 -0700

    pag productos, y funciones insertar, buscar, borrar, actualizar y mostrar

--- models/productos.js
+++ models/productos.js
@@ -1,36 +1,38 @@
 const  json = require('express/lib/response');
 const { connect, query } = require('../models/conexion.js');
 const conexion = require('../models/conexion.js'); 
-//const router = require('../routers/index.js');
-//const {Route} = require('router');
-
+const promise =  require('../models/conexion.js');
+const router = require('../routers/index.js');
+//const {Route} = require('routers');
 var productosDb = {};
-
 module.exports = productosDb;
-productosDb.insertar = function insertar(productos){
-    return new Promise((resolve,reject)=>{
-        var sqlConsulta = "insert into productos set ?";
-        conexion.query(sqlConsulta, productos, function(err,res){
-            if(err){
-                console.log("surgió un error" + err.message);
-                reject(err);
-            }else{
-                resolve({
-                    id: productos.id,
-                    descripcion: productos.descripcion,
-                    nombre: productos.nombre,
-                    precio: productos.precio,
-                    estado: productos.estado,
-                    imagen: productos.imagen
-                })
-            }
-        })
-    })
-}
+
+
+productosDb.insertar = function insertar(producto){
+  return new Promise((resolve,reject)=>{
+    var sqlConsulta = "insert into producto set ?";
+      conexion.query(sqlConsulta, producto, function(err,res){
+          if(err){
+              console.log("surgió un error" + err.message);
+              reject(err);
+          }else{
+              console.log('Producto insertado:', producto);
+              resolve({
+                id: producto.id,
+                descripcion: producto.descripcion,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                estado: producto.estado,
+                imagen: producto.imagen
+              });
+          }
+      });
+  });
+};
 
 productosDb.mostrarTodos = function mostrarTodos() {
     return new Promise((resolve, reject) => {
-      var sqlConsulta = "select * from productos";
+      var sqlConsulta = "select * from producto";
       conexion.query(sqlConsulta, null, function (err, res) {
         if (err) {
           console.log("Surgió un error " + err.message);
@@ -45,7 +47,7 @@ productosDb.mostrarTodos = function mostrarTodos() {
 //buscar alumno por matricula
 productosDb.buscarId = function buscarId(id){
     return new Promise((resolve,reject)=>{
-      var sqlConsulta = "SELECT * FROM productos WHERE id_producto = ?";
+      var sqlConsulta = "SELECT * FROM producto WHERE id = ?";
       conexion.query(sqlConsulta, [id], function(err, res){
         if(err){
           console.log("Surgió un error " + err.message);
@@ -66,12 +68,12 @@ productosDb.borrar = function borrar(id) {
     return new Promise((resolve, reject) => {
         productosDb.buscarId(id)
         .then(productos=> {
-          conexion.query('DELETE FROM productos WHERE id_producto = ?', [id], function(err, res) {
+          conexion.query('DELETE FROM producto WHERE id = ?', [id], function(err, res) {
             if (err) {
               console.log('Surgió un error ' + err.message);
               reject(err);
             } else {
-              resolve({ message: `User where matricula = ${id} deleted succesfully.` });
+              resolve({ message: `User where id = ${id} deleted succesfully.` });
             }
           });
         })
@@ -79,15 +81,29 @@ productosDb.borrar = function borrar(id) {
     });
   };
 
-  productosDb.actualizar = function(id, productos) {
+  productosDb.actualizar = function(id, producto) {
     return new Promise((resolve, reject) => {
-      const sql = 'UPDATE productos SET descripcion, nombre = ?, precio = ?, estado = ?, imagen = ? WHERE id = ?';
-      const params = [productos.descripcion, productos.nombre, productos.precio, productos.estado, productos.imagen, id];
+      const sql = 'UPDATE producto SET descripcion= ?, nombre = ?, precio = ?, estado = ?, imagen = ? WHERE id = ?';
+      const params = [producto.descripcion, producto.nombre, producto.precio, producto.estado, producto.imagen, id];
       conexion.query(sql, params, function(err, result) {
         if (err) {
           reject(err);
         } else {
-          resolve(`Producto con matrícula ${id} actualizado correctamente.`);
+          resolve(`Producto con id ${id} actualizado correctamente.`);
+        }
+      });
+    });
+  };
+
+  productosDb.mostrarProductos = function mostrarProductos() {
+    return new Promise((resolve, reject) => {
+      var sqlConsulta = "select * from producto";
+      conexion.query(sqlConsulta, null, function (err, res) {
+        if (err) {
+          console.log("Surgió un error " + err.message);
+          reject(err);
+        } else {
+          resolve(res);
         }
       });
     });
